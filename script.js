6c465529905ee4ede3518a65a46f117a26d914ba
Alsu Ismayilzade <131715293+alsu-ismayilzada@users.noreply.github.com>
2024-04-24 11:07:08 +0400

    updating

--- script.js
+++ script.js
@@ -8,43 +8,77 @@ let pRight = document.querySelector(".right .content p");
 let currentValue = "RUB";
 let selectedValue = "USD";
 let isActive = "inputLeft";
+inputLeft.value = 1;
 
-function updateConversion() {
-    if(isActive == "inputLeft" ){
-    fetch(`https://v6.exchangerate-api.com/v6/79761deae2c772e1188646a0/latest/${currentValue}`)
-    .then(res => res.json())
-    .then(data => {
-        inputRight.value = data.conversion_rates[selectedValue] * inputLeft.value;
-    });
-}else{
-    fetch(`https://v6.exchangerate-api.com/v6/79761deae2c772e1188646a0/latest/${selectedValue}`)
-        .then(res => res.json())
-        .then(data => {
-        inputLeft.value = data.conversion_rates[currentValue] * inputRight.value;
-    });
-}
-}
-
-currentCurrency.forEach(item => {
-    item.addEventListener("click", () => {
-        currentCurrency.forEach(el => {
-            el.style.backgroundColor = "";
-            el.style.color = ""}); 
-        item.style.backgroundColor = "rgb(131, 58, 224)";
-        item.style.color = "white";
-        currentValue = item.innerHTML;
-        updateConversion();
-        fetch(`https://v6.exchangerate-api.com/v6/79761deae2c772e1188646a0/latest/${currentValue}`)
+fetch(`https://v6.exchangerate-api.com/v6/9e7d284ce57596813ba6df0a/latest/${currentValue}`)
         .then(res => res.json())
         .then(data => {
          pLeft.innerHTML= `1 ${currentValue} = ${data.conversion_rates[selectedValue]} ${selectedValue} `
        })
-       fetch(`https://v6.exchangerate-api.com/v6/79761deae2c772e1188646a0/latest/${selectedValue}`)
+       fetch(`https://v6.exchangerate-api.com/v6/9e7d284ce57596813ba6df0a/latest/${selectedValue}`)
         .then(res => res.json())
         .then(data => {
          pRight.innerHTML= `1 ${selectedValue} = ${data.conversion_rates[currentValue]} ${currentValue} `
        })
 
+function updateConversion() {
+    console.log("A request is sent to the server");
+    if(isActive == "inputLeft" ){
+    fetch(`https://v6.exchangerate-api.com/v6/9e7d284ce57596813ba6df0a/latest/${currentValue}`)
+    .then(res => res.json())
+    .then(data => {
+        let x = (data.conversion_rates[selectedValue] * inputLeft.value).toString();
+        let decimalIndex = x.indexOf('.');
+        if (decimalIndex === -1) {
+            inputRight.value = (data.conversion_rates[selectedValue] * inputLeft.value).toFixed(5);
+        } else {
+            inputRight.value = x.slice(0, decimalIndex + 6);
+        }
+        pLeft.innerHTML= `1 ${currentValue} = ${data.conversion_rates[selectedValue]} ${selectedValue} `
+    });
+}else{
+    fetch(`https://v6.exchangerate-api.com/v6/9e7d284ce57596813ba6df0a/latest/${selectedValue}`)
+        .then(res => res.json())
+        .then(data => {
+            let x = (data.conversion_rates[currentValue] * inputRight.value).toString();
+        let decimalIndex = x.indexOf('.');
+        if (decimalIndex === -1) {
+            inputLeft.value = (data.conversion_rates[currentValue] * inputRight.value).toFixed(5);
+        } else {
+            inputLeft.value = x.slice(0, decimalIndex + 6);
+        }
+        pLeft.innerHTML= `1 ${currentValue} = ${data.conversion_rates[selectedValue]} ${selectedValue} `
+    });
+}
+}
+function updateInfo(){
+    fetch(`https://v6.exchangerate-api.com/v6/9e7d284ce57596813ba6df0a/latest/${currentValue}`)
+    .then(res => res.json())
+    .then(data => {
+     pLeft.innerHTML= `1 ${currentValue} = ${data.conversion_rates[selectedValue]} ${selectedValue} `
+   })
+   fetch(`https://v6.exchangerate-api.com/v6/9e7d284ce57596813ba6df0a/latest/${selectedValue}`)
+    .then(res => res.json())
+    .then(data => {
+     pRight.innerHTML= `1 ${selectedValue} = ${data.conversion_rates[currentValue]} ${currentValue} `
+   })
+}
+
+currentCurrency.forEach(item => {
+    item.addEventListener("click", () => {
+        currentCurrency.forEach(el => {
+            el.classList.remove("purple-button")}); 
+        item.classList.add("purple-button");
+        currentValue = item.innerHTML;
+        if(currentValue != selectedValue){
+            updateConversion();
+            updateInfo();
+        }else{
+            console.log("No request is sent to the server");
+            inputRight.value = inputLeft.value;
+            pLeft.innerHTML= `1 ${currentValue} = 1 ${selectedValue} `;
+            pRight.innerHTML = pLeft.innerHTML;
+           }
     });
     
 });
@@ -52,41 +86,44 @@ currentCurrency.forEach(item => {
 selectedCurrency.forEach(item => {
     item.addEventListener("click", () => {
         selectedCurrency.forEach(el => {
-            el.style.backgroundColor = "";
-            el.style.color = ""}); 
-        item.style.backgroundColor = "rgb(131, 58, 224)";
-        item.style.color = "white";
+            el.classList.remove("purple-button")}); 
+        item.classList.add("purple-button");
         selectedValue = item.innerHTML;
+    if(currentValue != selectedValue){
         updateConversion();
-        fetch(`https://v6.exchangerate-api.com/v6/79761deae2c772e1188646a0/latest/${currentValue}`)
-        .then(res => res.json())
-        .then(data => {
-         pLeft.innerHTML= `1 ${currentValue} = ${data.conversion_rates[selectedValue]} ${selectedValue} `
-       })
-       fetch(`https://v6.exchangerate-api.com/v6/79761deae2c772e1188646a0/latest/${selectedValue}`)
-        .then(res => res.json())
-        .then(data => {
-         pRight.innerHTML= `1 ${selectedValue} = ${data.conversion_rates[currentValue]} ${currentValue} `
-       })
+        updateInfo();
+    }else{
+        console.log("No request is sent to the server");
+        inputRight.value = inputLeft.value;
+        pLeft.innerHTML= `1 ${currentValue} = 1 ${selectedValue} `;
+        pRight.innerHTML = pLeft.innerHTML;
+       }
     });
 });
 inputLeft.addEventListener("input",()=>{
     if( /^(\d*\.?\d*|\d*,?\d*)$/.test(inputLeft.value)){
+        isActive = "inputLeft";
       if(inputLeft.value.indexOf(',') != -1){
         inputLeft.value = inputLeft.value.split(",").join(".");
         console.log(inputLeft.value);
-        isActive = "inputLeft";
       }
       updateConversion();
-
     }else{
         inputLeft.value=inputLeft.value.slice(0, inputLeft.value.length-1);
     }
-    
 })
+
 inputRight.addEventListener("input",()=>{
-    isActive = "inputRight";
-    updateConversion();
+    if( /^(\d*\.?\d*|\d*,?\d*)$/.test(inputRight.value)){
+        isActive = "inputRight";
+        if(inputRight.value.indexOf(',') != -1){
+          inputRight.value = inputRight.value.split(",").join(".");
+          console.log(inputRight.value);
+        }
+        updateConversion();
+      }else{
+        inputRight.value=inputRight.value.slice(0, inputRight.value.length-1);
+      }
 })
 
 updateConversion();
